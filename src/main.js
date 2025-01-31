e1eccd5b89bbff4649c0a5064e56efdc91b0c4a3
PruthviLahare <142437821+PruthviLahare@users.noreply.github.com>
2025-01-31 12:42:38 +0530

    order packed succcessfully

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
+// const deliveringOrder
+
+const packingFood = (obj) => {
+  console.log("Packing order...");
+
+  setTimeout(() => {
+    obj["packageDetails"] = "Packed in eco-friendly box";
+    console.log("Order packed:", obj);
+    // deliveringOrder(obj);
+  }, 2000);
+};
+
 const preparingFood = (orderId, obj, ms) => {
   console.log("preparing food...");
 
   setTimeout(() => {
     console.log("Food is ready:", obj);
+    packingFood(obj);
   }, 3000);
 };
 
@@ -12,7 +25,7 @@ const main = () => {
   console.log(`[0.00s] Order received: { orderId: 123 }`);
 
   const obj = { orderId: 123, foodDetails: "Burger & Fries" };
-  preparingFood(123, obj, ms);
+  return preparingFood(123, obj, ms);
 };
 
 main();
