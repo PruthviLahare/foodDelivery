dde3642af391ff603be7219f5c39644dfb7df740
PruthviLahare <142437821+PruthviLahare@users.noreply.github.com>
2025-02-04 11:21:17 +0530

    bill tracked successfully

--- src/foodDelivery.js
+++ src/foodDelivery.js
@@ -1,44 +1,48 @@
-const process = (before, after, timeNeeded, next) => {
+const process = (before, update, complete, timeNeeded, next) => (bill) => {
   before();
 
   setTimeout(() => {
-    after();
-    next();
+    update(bill);
+    complete(bill);
+
+    next(bill);
   }, timeNeeded);
 };
 
-const preparing = () => {
-  process(
-    () => console.log("Preparing food..."),
-    () => console.log("food is ready"),
-    2000,
-    packing
-  );
-};
+const delivery = process(
+  () => console.log("Delivering order..."),
+  (bill) => (bill["packageDetails"] = "'Delivered by John at 7:30 PM"),
+  () => console.log("deliveryDetails"),
+  5000,
+  Deno.exit
+);
 
-const packing = () => {
-  process(
-    () => console.log("Packing order..."),
-    () => console.log("Order packed"),
-    2000,
-    delivery
-  );
-};
+const packing = process(
+  () => console.log("Packing order..."),
+  (bill) => (bill["packageDetails"] = "Packed in eco-friendly box"),
+  () => console.log("Order packed"),
+  2000,
+  delivery
+);
 
-const delivery = () => {
-  process(
-    () => console.log("Delivering order..."),
-    () => console.log("Order delivered"),
-    2000,
-    Deno.exit
-  );
-};
+const preparing = process(
+  () => console.log("Preparing food..."),
+  (bill) => (bill["foodDetails"] = "Burger & Fries"),
+  (bill) => console.log("food is ready:", bill),
+  3000,
+  packing
+);
+
+const orderRecived = process(
+  () => console.log("Delivering order..."),
+  (bill) => (bill["id"] = 123),
+  (bill) => console.log("Order received", bill),
+  0,
+  preparing
+);
 
 const main = () => {
-  const bill = { orderId: 123 };
-  console.log("Order received:", bill);
-
-  preparing(bill);
+  orderRecived({ id: 123 });
 };
 
 main();
